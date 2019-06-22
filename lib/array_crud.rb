50fa1e6089a5cf54f54eff381c02c93aeaeb1d6b
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-06-22 17:55:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,9 +26,9 @@ def remove_element_from_start_of_array(array)
   array.shift  
 end
 
-def retrieve_element_from_index(array, 2)
+def retrieve_element_from_index(array, index_number)
   array = ["am"]
-  array[index_number]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
